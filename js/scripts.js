b731e5c4fc0fdbe0136be54d361fb6db30ac9b75
Andrew Niekamp and Rick Thornbrugh <andrew+rick@>
2016-06-22 10:45:31 -0700

    add user interface output

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-// $(document).ready(function() {
-//   $("form#leap-year").submit(function(event) {
-//     event.preventDefault();
-//     var year = parseInt($("input#year").val());
-//     var result = leapYear(year);
-//     $("#result").text(result);
-//   });
-// });
+// business logic
 var leapYear = function(year) {
   if ((year % 4 === 0)  && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
@@ -14,26 +7,22 @@ var leapYear = function(year) {
   }
 };
 
-// var leapYear = function(year) {
-//   if (year % 4 === 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
 
 // jQuery user interface logic here.
-
-
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
-
-
-
-// jQuery user interface logic here.
